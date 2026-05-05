067e2a39becc59746d51d839ea2ea4b30392b5cb
Harrison Fabian <harrisoncfabian@gmail.com>
2026-05-04 21:30:26 -0500

    cleanup

--- src/tests.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('prompt_pieces.yml','r') as file:
-    config = yaml.safe_load(file)
-
-    for question in config['questions']:
-        print(question)
-
-    print(config['prompt'])
